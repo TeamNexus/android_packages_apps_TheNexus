b354f357ca690195bbcc8ba579091be557b55bb3
Lukas0610 <mail@lukasberger.at>
2017-05-02 17:14:50 +0200

    updated Android.mk

--- app/src/main/Android.mk
+++ app/src/main/Android.mk
@@ -15,32 +15,29 @@
 #
 
 LOCAL_PATH:= $(call my-dir)
-
 include $(CLEAR_VARS)
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    android-support-v4 \
-    android-support-v7-appcompat \
-    android-support-v13 \
-    android-support-design
 
-LOCAL_SRC_FILES := \
-    $(call all-java-files-under, app/src/main/java)
-
-LOCAL_RESOURCE_DIR := \
-    $(LOCAL_PATH)/app/src/main/res \
-	prebuilts/sdk/current/support/v7/appcompat/res \
-	prebuilts/sdk/current/support/design/res
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
+    frameworks/support/design/res \
+    frameworks/support/v7/appcompat/res
 
 LOCAL_AAPT_FLAGS := \
-	--auto-add-overlay \
-	--extra-packages android.support.v7.appcompat
+    --auto-add-overlay \
+    --extra-packages android.support.design \
+    --extra-packages android.support.v7.appcompat
 
-LOCAL_MODULE_TAGS        := optional
+LOCAL_STATIC_JAVA_LIBRARIES += \
+    android-support-design \
+    android-support-v4 \
+    android-support-v7-appcompat
+
+LOCAL_SRC_FILES := $(call all-java-files-under, java)
+
 LOCAL_PACKAGE_NAME       := TheNexus
 LOCAL_OVERRIDES_PACKAGES := TheNexus-prebuilt
+LOCAL_CERTIFICATE        := platform
 LOCAL_PRIVILEGED_MODULE  := true
-LOCAL_PROGUARD_ENABLED:  := disabled
-LOCAL_SDK_VERSION        := current
-LOCAL_MIN_SDK_VERSION    := 25
+
+#LOCAL_PROGUARD_FLAG_FILES := $(LOCAL_PATH)/../../proguard-rules.pro
 
 include $(BUILD_PACKAGE)
